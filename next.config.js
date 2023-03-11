6ef6af625ff696dbe0a56ea4a99583995a7126ef
Aditya <2adijbp@gmail.com>
2023-03-11 10:40:06 -0800

    Removing unecessary things

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    BACKEND_URL: process.env.BACKEND_URL,
+    API_KEY: process.env.API_KEY,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(process.env.BACKEND_URL, process.env.API_KEY);
 
 export default function Home() {
   return (
@@ -14,7 +13,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}></main>
+      <main></main>
     </>
   );
 }
