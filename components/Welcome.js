3c38aab31a832610d24015183fc47f8e35f57be4
Aditya <2adijbp@gmail.com>
2023-03-11 12:30:02 -0800

    What we have rn

---
+++ components/Welcome.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import { Container, Grid, Box, Typography } from "@mui/material";
+import axios from "axios";
+import { useSpring, animated } from "@react-spring/web";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(process.env.BACKEND_URL, process.env.API_KEY);
+
+export default function Welcome() {
+  const [props, api] = useSpring(
+    () => ({
+      from: { fontSize: "0px" },
+      to: { fontSize: "10vw" },
+    }),
+    []
+  );
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      {/* <animated.div style={{ fontFamily: "Larsseit-ExtraBold" }}>
+        Welcome to Project Buddy!
+      </animated.div> */}
+      <div style={{ fontFamily: "Larsseit-ExtraBold" }}>
+        Welcome to Project Buddy!
+      </div>
+    </Box>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
+import React, { useState } from "react";
 import Head from "next/head";
+import Carousel from "react-simply-carousel";
 
-import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient(process.env.BACKEND_URL, process.env.API_KEY);
+import Welcome from "../components/Welcome";
 
 export default function Home() {
+  const [slide, setSlide] = useState(0);
+
   return (
     <>
       <Head>
@@ -13,7 +15,44 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main></main>
+      {/* <main sx={{ m: 0, p: 0 }}>
+
+      </main> */}
+      <Carousel
+        containerProps={{
+          style: {
+            width: "100%",
+            margin: 0,
+            padding: 0,
+          },
+        }}
+        preventScrollOnSwipe
+        swipeTreshold={60}
+        activeSlideIndex={slide}
+        activeSlideProps={{
+          style: {
+            background: "blue",
+            width: "100vw",
+            height: "100vh",
+          },
+        }}
+        onRequestChange={setSlide}
+        forwardBtnProps={{
+          style: {
+            display: "none",
+          },
+        }}
+        backwardBtnProps={{
+          style: {
+            display: "none",
+          },
+        }}
+        itemsToShow={1}
+        speed={400}
+      >
+        <div>Jeeho Oppa</div>
+        <div>Jayden Oppa</div>
+      </Carousel>
     </>
   );
 }
