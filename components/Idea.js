cca57aed021887191e8a7489360d086ff41cdccd
Aditya <2adijbp@gmail.com>
2023-03-12 01:09:12 -0800

    Idea page jayden

--- components/Idea.js
+++ components/Idea.js
@@ -7,6 +7,8 @@ import { Trail } from "./Trail";
 import { MyTextField } from "./MyTextField";
 
 import styles from "../styles/pages.module.css";
+import jayden1 from "../assets/images/PontificateTransparent.png";
+import jayden2 from "../assets/images/SnackTimeTransparent.png";
 
 export default function Idea({ goPrev, goNext, slide, idea, setIdea }) {
   return (
@@ -17,9 +19,12 @@ export default function Idea({ goPrev, goNext, slide, idea, setIdea }) {
         </Trail>
         <JaydenPrompt
           text={
-            "OK! No funny business… Describe your project idea to me. I like to think about who my target audience is and the goals!"
+            "OK! No funny business… Describe your project idea to me. I like to think about who my target audience is and the goals! I’ll eat a snack while you’re thinking about it..."
           }
           open={slide == 2}
+          firstImage={jayden1}
+          secondImage={jayden2}
+          timing={11000}
         />
         <Stack className={styles.textbox}>
           <MyTextField
